dace439514caa9a7533ea3c22655199137f61d32
oladiiposaheed <64591844+oladiiposaheed@users.noreply.github.com>
2023-09-09 17:24:38 +0100

    Delete testapp directory

--- testapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from testapp.models import StudentInfo3
-# Register your models here.
-class StudentInfoAdmin3(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'age', 'gender', 'math', 'chem', 'phy', 'td', 'bio', 'eng']
-
-admin.site.register(StudentInfo3, StudentInfoAdmin3)
